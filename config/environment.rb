a5c036ea616aed5ec61c95eedf07cd5e1155eafd
Metalab <pablo@metalab.harvard.edu>
2014-02-20 17:02:53 -0500

    trying to force production environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+ENV['RAILS_ENV'] ||= 'production'
+
 # Initialize the Rails application.
 Curarium::Application.initialize!
 

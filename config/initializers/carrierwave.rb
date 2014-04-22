ae89664b1cad99e16cfd9430c66ef617e4f6afe4
Metalab <pablo@metalab.harvard.edu>
2014-04-22 16:12:59 -0400

    the line "response = Rails.cache.fetch(params.to_s) { eval(params[:type]) }" had been changed to "response = Rails.cache.fetch("all") { eval(params[:type]) }". Was there a reason for that? It caused major caching problems.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,4 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAIPWG3CCQNAKATBLQ',
-    :aws_secret_access_key  => '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'
-  }
-  config.fog_directory  = "curarium_uploads"
-end
-
-=begin
-CarrierWave.configure do |config|
   
   if Rails.env.development? || Rails.env.test?
     config.storage = :file
@@ -21,4 +11,3 @@ CarrierWave.configure do |config|
     config.fog_directory  = "curarium_uploads"
   end
 end
-=end

ca37248670bce86ed3eb2a0616ad483489765bc2
Metalab <pablo@metalab.harvard.edu>
2014-04-30 12:57:42 -0400

    -a

--- app/workers/visualizer.rb
+++ app/workers/visualizer.rb
@@ -1,25 +1,12 @@
-class VisualizationsController < ApplicationController
-  skip_before_action :authorize, only: [:index]
-  
-  def index
-    @collection = Collection.find(params[:collection_id])
-    @properties = @collection.configuration.keys
-    respond_to do |format|
-      format.html { render action: "index" }
-      format.js { render action: "index" }
-      format.json do
-        response = VizCache.find_by(query: encode_params)
-        if response.nil?
-          Visualizer.new.async.perform(params)
-          #response = eval(params[:type])
-          #stored_response = VizCache.new({query: encode_params, data: response})
-          #stored_response.save
-        else
-          response = response.data
-        end
-        render json: response
-      end
-    end
+class Visualizer
+  include SuckerPunch::Job
+
+  def perform(params)
+   @params = params
+   response = eval(params[:type])
+   stored_response = VizCache.new({query: encode_params, data: response})
+   stored_response.save
+   return response
   end
   
   def tag
@@ -46,13 +33,13 @@ class VisualizationsController < ApplicationController
   end
   
   def treemap
-    minimum = params[:minimum].to_i || 0
-    @collection = Collection.find(params[:collection_id])
+    minimum = @params[:minimum].to_i || 0
+    @collection = Collection.find(@params[:collection_id])
     @records = @collection.records.where("lower(parsed->:field) LIKE :tag", {
-      field: params[ :type ],
-      tag: '%\"' + params[ :property ] + '\"%'
+      field: @params[ :type ],
+      tag: '%\"' + @params[ :property ] + '\"%'
     })
-    query = @collection.sort_properties(params[:include],params[:exclude],params[:property], minimum)
+    query = @collection.sort_properties(@params[:include],@params[:exclude],@params[:property], minimum)
     tmap = treemapify(query[:properties])
     length = query[:length]
     return {length: length, treemap: tmap, properties: query, records: @records}
@@ -74,8 +61,8 @@ class VisualizationsController < ApplicationController
   end
   
   def thumbnail
-    @collection = Collection.find(params[:collection_id])
-    records = @collection.query_records(params[:include],params[:exclude])
+    @collection = Collection.find(@params[:collection_id])
+    records = @collection.query_records(@params[:include],@params[:exclude])
     thumbnails = []
     records.each do |thumb|
       parsed = {}
@@ -98,44 +85,37 @@ class VisualizationsController < ApplicationController
   end
   
   def properties
-    @collection = Collection.find(params[:collection_id])
-    return @collection.sort_properties(params[:include],params[:exclude],params[:property])[:properties].keys
+    @collection = Collection.find(@params[:collection_id])
+    return @collection.sort_properties(@params[:include],@params[:exclude],@params[:property])[:properties].keys
   end
   
   def list_records
-    @collection = Collection.find(params[:collection_id])
-    records = @collection.list_query(params[:include],params[:exclude])
+    @collection = Collection.find(@params[:collection_id])
+    records = @collection.list_query(@params[:include],@params[:exclude])
     return records
   end
-  
-  def encode_params
-    collection = params[:collection_id].to_s
-    type = params[:type].to_s
-    property = params[:property].to_s
+ 
+ def encode_params
+    collection = @params[:collection_id].to_s
+    type = @params[:type].to_s
+    property = @params[:property].to_s
     include = ""
-    unless params[:include].nil?
-      params[:include].sort.each do |i|
+    unless @params[:include].nil?
+      @params[:include].sort.each do |i|
         include += i.to_s
       end
     end
     exclude = ""
-    unless params[:exclude].nil?
-      params[:exclude].sort.each do |e|
+    unless @params[:exclude].nil?
+      @params[:exclude].sort.each do |e|
         exclude += e.to_s
       end
     end
     return ('c'+collection+'_'+type+'_'+property+'_'+include+'_'+exclude).gsub(/:/,'_')
   end
   
-=begin  
-  THIS WAS PART OF AN ILL FATED ATTEMPT TO MAKE VISUALIZATIONS EMBEDDABLE.
-  def embed
-    url = URI.parse('http://curarium.herokuapp.com/collections/1/visualizations?type=treemap&property=topics')
-    req = Net::HTTP::Get.new(url.to_s)
-    res = Net::HTTP.start(url.host, url.port) {|http|
-       http.request(req)
-    }
-    render text: res.body
-  end
-=end  
 end
+
+
+
+  

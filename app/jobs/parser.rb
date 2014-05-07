d4392b6f1f6ac12db415b38d7deec780ee2e75be
Metalab <pablo@metalab.harvard.edu>
2014-05-07 16:57:47 -0400

    using temporary folder to unzip and parse records

--- app/jobs/parser.rb
+++ app/jobs/parser.rb
@@ -1,41 +1,49 @@
 class Parser
   include SuckerPunch::Job
-  #LogJob.new.async.perform
-
-  def perform(collection_id)
+  require 'zip'
+  
+  def perform(collection_id, zip_path)
+    paths = []
+    Zip::File.open(zip_path) do |zip_file|
+      zip_file.each do |entry|
+        file_path = "#{Rails.root}/tmp/#{entry.name}"
+        zip_file.extract(entry, file_path)
+        paths << file_path
+      end
+    end
+    paths.shift
     collection = Collection.find(collection_id)
-    puts collection.name
-    ent = collection.json_files
-    puts "Ingesting #{ent.count} records collection: #{collection.name}"
+    puts "Ingesting #{paths.count} records collection: #{collection.name}"
     configuration = collection.configuration
 
 
     j_count = 0
-    ent.each { |f| 
-      read_record( f.path, configuration )
-      t = Thread.new { read_record( f.path, configuration ) }
+    paths.each { |path| 
+      read_record( path, configuration )
+      t = Thread.new { read_record( path, configuration ) }
 
       t.join
 
       ok = collection.create_record_from_parsed t[ :original ], t[ :parsed ] unless t[ :original ].nil?
       if ok
         j_count += 1
-        f.destroy
       end
       
       if j_count > 0 && j_count % 100 == 0
         puts j_count
-
+        File.delete(path)
         GC.start
         puts "heap: #{GC.stat[ :heap_live_num ]}"
       end
     }
-
-    puts "Processed #{j_count} JSON files (out of #{ent.count} total files in directory)" 
+    File.delete(zip_path)
+    collection.approved = True
+    collection.save
+    puts "Processed #{j_count} JSON files (out of #{paths.count} total files in directory)" 
   end
   
   def read_record( path, configuration )
-    json_file = JSON.parse(Net::HTTP.get(URI(path.to_s)))
+    json_file = JSON.parse(File.open(path, 'r').read)
     pr = {}
     configuration.each do |field|
       pr[field[0]] = Collection.follow_json(json_file, field[1])

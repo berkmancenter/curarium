3886c501734e4535a6eee0fa571b9890d60630fd
Ryan Westphal <ryan@trippingthebits.com>
2014-07-01 16:43:52 -0400

    #8945 - controller forgot which open to use for some reason

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class RecordsController < ApplicationController
   before_action :set_record, only: [:show, :thumb, :edit, :update, :destroy]
   skip_before_action :authorize, only: [:show]

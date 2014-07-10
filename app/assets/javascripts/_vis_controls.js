f3f2bb2d39c50e58fb87254bb30933a1dbe81829
Ryan Westphal <ryan@trippingthebits.com>
2014-07-10 10:29:58 -0400

    #8986 - factor out prop html to function

--- app/assets/javascripts/_vis_controls.js
+++ app/assets/javascripts/_vis_controls.js
@@ -9,13 +9,12 @@ $( function( ) {
 
     for (var i = 0; i < vals.length; i++) {
       kv = vals[i].split("=");
+      v = kv[1].split(":");
+
       if (kv[0]=='include[]') {
-        v = kv[1].split(":");
-        propsHtml += "<span class='include'><a>x</a><input name='include[]' value='"+kv[1]+"' class='checkbox_hack'> "+v[1]+"</span> ";
-      }
-      else if (kv[0]=='exclude[]') {
-        v=kv[1].split(":");
-        propsHtml += "<span class='exclude'><a>x</a><input name='exclude[]' value='"+kv[1]+"' class='checkbox_hack'> "+v[1]+"</span> ";
+        propsHtml += propHtml( 'include', kv[1], v[1] );
+      } else if (kv[0]=='exclude[]') {
+        propsHtml += propHtml( 'exclude', kv[1], v[1] );
       }
     }
 
@@ -28,11 +27,16 @@ $( function( ) {
       var className = $( this ).data( 'cmd' );
       var value = sel+":"+val;
 
-      props.append( "<span class='"+className+"'><a>x</a><input class='checkbox_hack' name='"+className+"[]' value='"+value+"'> "+val+"</span> " );
+      props.append( propHtml( className, value, val ) );
     } );
 
     props.on( 'click', 'a', function() {
       $( this ).parent().remove();
+      return false;
     } );
+
+    function propHtml( className, value, text ) {
+      return '<span class="' + className + '"><a href="#">x</a> <input class="checkbox_hack" name="' + className + '[]" value="' + value + '">' + text + '</span>';
+    }
   }
 } );

aafe0594c2dcf4f0d27add44992257f70befcfe7
Ryan Westphal <ryan@trippingthebits.com>
2014-07-10 08:35:47 -0400

    #8986 - tidy remove code

--- app/assets/javascripts/_vis_controls.js
+++ app/assets/javascripts/_vis_controls.js
@@ -12,12 +12,12 @@ $( function( ) {
       if (kv[0]=='include[]') {
         id = kv[1].replace(/\s/g,'_');
         v = kv[1].split(":");
-        propsHtml += "<span id='"+id+"' class='include'><a onClick='removeEl(\""+id+"\")'>x</a><input name='include[]' value='"+kv[1]+"' class='checkbox_hack'> "+v[1]+"</span> ";
+        propsHtml += "<span id='"+id+"' class='include'><a>x</a><input name='include[]' value='"+kv[1]+"' class='checkbox_hack'> "+v[1]+"</span> ";
       }
       else if (kv[0]=='exclude[]') {
         id=kv[1].replace(/\s/g,'_');
         v=kv[1].split(":");
-        propsHtml += "<span id='"+id+"' class='exclude'><a onClick='removeEl(\""+id+"\")'>x</a><input name='exclude[]' value='"+kv[1]+"' class='checkbox_hack'> "+v[1]+"</span> ";
+        propsHtml += "<span id='"+id+"' class='exclude'><a>x</a><input name='exclude[]' value='"+kv[1]+"' class='checkbox_hack'> "+v[1]+"</span> ";
       }
     }
 
@@ -35,14 +35,11 @@ $( function( ) {
     var value = sel+":"+val;
     var id=val.replace(/\s/g,'_');
 
-    props.append( "<span id='"+id+"' class='"+className+"'><a onClick='removeEl(\""+id+"\")'>x</a><input class='checkbox_hack' name='"+className+"[]' value='"+value+"'> "+val+"</span> " );
+    props.append( "<span id='"+id+"' class='"+className+"'><a>x</a><input class='checkbox_hack' name='"+className+"[]' value='"+value+"'> "+val+"</span> " );
   }
 
-  function removeEl(id) {
-    el = document.getElementById(id);
-    if (el!=null) {
-      el.parentNode.removeChild(el);
-    }
-  }
+  props.on( 'click', 'a', function() {
+    $( this ).parent().remove();
+  } );
 
 } );

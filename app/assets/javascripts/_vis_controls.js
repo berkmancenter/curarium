68ee6b9bcd806d09e705a1fd1b02e6caeeebd238
Ryan Westphal <ryan@trippingthebits.com>
2014-08-06 15:48:30 -0400

    #9503 - turn vis_controls into a jQuery plugin

--- app/assets/javascripts/_vis_controls.js
+++ app/assets/javascripts/_vis_controls.js
@@ -1,86 +1,98 @@
-$( function( ) {
-  if ( $( '.vis-controls' ).length ) {
-    
-    function escapeSpecialChars ( str ) {
-      var from = ['<','>','\'','"'];
-      var to = ['&lt;','&gt;','&#39;','&quot;'];
-      for (var i = 0; i < from.length; i++) {
-        str = str.replace( from[i], to[i] );
-      }
-      return str;
-    }
+;( function ( $, undefined ) {
+  var _defaults = {
+  };
 
-    function propHtml( className, value, text ) {
+  var _options = { };
+
+  function escapeSpecialChars ( str ) {
+    var from = ['<','>','\'','"'];
+    var to = ['&lt;','&gt;','&#39;','&quot;'];
+    for (var i = 0; i < from.length; i++) {
+      str = str.replace( from[i], to[i] );
+    }
+    return str;
+  }
+
+  $.visControls = {
+    init: function( options ) {
+      _options = $.extend( { }, _defaults, options );
+
+      var props = $( '#props' );
+
+      var search = window.location.search.replace("?","");
+      if ( search.length ) {
+        var vals = decodeURIComponent(search).split("&");
+        var propsHtml = '';
+
+        var kv, v;
+
+        for (var i = 0; i < vals.length; i++) {
+          kv = vals[i].split("=");
+          v = kv[1].split(":");
+
+          if (kv[0]=='include[]') {
+            propsHtml += $.visControls.propHtml( 'include', kv[1], v[1] );
+          } else if (kv[0]=='exclude[]') {
+            propsHtml += $.visControls.propHtml( 'exclude', kv[1], v[1] );
+          } else if (kv[0]=='property') {
+            $( '#property' ).val( v[0] );
+          }
+        }
+
+        props.html( propsHtml );
+      }
+
+      $( '#per_page' ).change( function() {
+        $( '#vis-form' ).submit();
+      } );
+
+
+      $( '#vis' ).change ( function() {
+        if ($.inArray(this.value,['treemap'])>-1) {
+          $( '#property' ).prop('disabled',false);
+        }
+        else {
+          $( '#property' ).prop('disabled',true);
+        }
+
+        if ($.inArray(this.value,['thumbnails','list'])>-1) {
+          $( '.page_in' ).prop('disabled',false);
+        }
+        else {
+          $( '.page_in' ).prop('disabled',true);
+        }
+      } );
+
+      $( '#vis' ).trigger('change')
+
+      $( '.add-prop' ).click( function() {
+        var sel = $( '#selprop' ).val();
+        var val = $( '#propval' ).val();
+
+        if (val!=='') {
+          var className = $( this ).data( 'cmd' );
+          var value = sel+":"+val;
+          props.append( $.visControls.propHtml( className, value, val ) );
+          $( '#propval' ).val('');
+        }
+      } );
+
+      props.on( 'click', 'a', function() {
+        $( this ).parent().remove();
+        return false;
+      } );
+    },
+
+    propHtml: function( className, value, text ) {
       className = escapeSpecialChars( className );
       value = escapeSpecialChars( value );
       text = escapeSpecialChars ( text );
       return '<span class="' + className + '"><a href="#">x</a> <input class="checkbox_hack" name="' + className + '[]" value="' + value + '">' + text + '</span>';
     }
+  };
 
-      
-
-    var props = $( '#props' );
-
-    var search = window.location.search.replace("?","");
-    if ( search.length ) {
-      var vals = decodeURIComponent(search).split("&");
-      var propsHtml = '';
-
-      var kv, v;
-
-      for (var i = 0; i < vals.length; i++) {
-        kv = vals[i].split("=");
-        v = kv[1].split(":");
-
-        if (kv[0]=='include[]') {
-          propsHtml += propHtml( 'include', kv[1], v[1] );
-        } else if (kv[0]=='exclude[]') {
-          propsHtml += propHtml( 'exclude', kv[1], v[1] );
-        }
-      }
-
-      props.html( propsHtml );
-    }
-
-    $( '#per_page' ).change( function() {
-      $( '#vis-form' ).submit();
-    } );
-
-
-    $( '#vis' ).change ( function() {
-      if ($.inArray(this.value,['treemap'])>-1) {
-        $( '#property' ).prop('disabled',false);
-      }
-      else {
-        $( '#property' ).prop('disabled',true);
-      }
-
-      if ($.inArray(this.value,['thumbnails','list'])>-1) {
-        $( '.page_in' ).prop('disabled',false);
-      }
-      else {
-        $( '.page_in' ).prop('disabled',true);
-      }
-    } );
-
-    $( '#vis' ).trigger('change')
-
-    $( '.add-prop' ).click( function() {
-      var sel = $( '#selprop' ).val();
-      var val = $( '#propval' ).val();
-
-      if (val!=='') {
-        var className = $( this ).data( 'cmd' );
-        var value = sel+":"+val;
-        props.append( propHtml( className, value, val ) );
-        $( '#propval' ).val('');
-      }
-    } );
-
-    props.on( 'click', 'a', function() {
-      $( this ).parent().remove();
-      return false;
-    } );
-
+  if ( $( '.vis-controls' ).length ) {
+    $.visControls.init();
   }
-} );
+} ) ( window.jQuery );
+

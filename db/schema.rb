45c569ac9e0bc96c8698b40ac390f28aaf5f7e35
Ryan Westphal <ryan@trippingthebits.com>
2014-11-05 16:42:56 -0500

    #10458 - remove old non-relational fields from tray model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105203204) do
+ActiveRecord::Schema.define(version: 20141105213313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,8 +119,6 @@ ActiveRecord::Schema.define(version: 20141105203204) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
-    t.integer  "works",          default: [], array: true
-    t.json     "visualizations", default: []
   end
 
   create_table "users", force: true do |t|

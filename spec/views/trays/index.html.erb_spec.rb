fc119b2285c0bfcc4468c7b39b9ce65cd6f2c0c6
Ryan Westphal <ryan@trippingthebits.com>
2014-11-12 18:32:14 -0500

    #10172 - tray index header

--- spec/views/trays/index.html.erb_spec.rb
+++ spec/views/trays/index.html.erb_spec.rb
@@ -3,6 +3,18 @@ require 'spec_helper'
 describe ( 'trays/index' ) {
   subject { rendered }
 
-  before {
+  context ( 'user with tray' ) {
+    let ( :user ) { User.first }
+    let ( :trays ) { user.trays }
+
+    before {
+      assign( :owner, user )
+      assign( :trays, trays )
+      render
+    }
+
+    it {
+      should have_css 'h1', text: 'Tray Manager'
+    }
   }
 }

53ff71db768ea66586ff407df92f118d86cfb45c
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2014-12-15 15:49:27 -0500

    users/show - added tray preview & collections

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -8,10 +8,33 @@ describe ( 'users/show' ) {
   before {
     assign( :user, user )
     session[ :user_id ] = user.id
+
+    assign( :trays, user.trays )
     render
   }
 
   it {
     should have_css 'h1.collection-header', text: user.name
   }
+
+  it {
+    should have_css 'section.user-trays'
+  }
+
+  it {
+    should have_css 'h1.collection-header', text: 'Trays'
+  }
+
+  it {
+    should have_css '.tray-preview'
+  }
+
+  it {
+    should have_css 'section.user-collections'
+  }
+
+  it {
+    should have_css 'h1.collection-header', text: 'Collections'
+  }
+
 }

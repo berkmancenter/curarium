5a0f087af438cfe1a2e1b96178304cba198f3f19
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-01-21 14:43:33 -0500

    #10461 - tests for spotlight model

---
+++ spec/models/spotlight_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe ( 'Spotlight model' ) {
+  context ( 'with valid data' ) {
+    let ( :s ) { Spotlight.first }
+
+    it { s.should be_valid }
+
+    it { s.should respond_to :title, :body }
+
+    it { s.should respond_to :user }
+
+    it { s.should respond_to :waku_url }
+
+  }
+}

396e9085ee6ff4c174f267a391cb62fece8d71a1
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-02-11 14:32:06 -0500

    #7721 - user sidebar with profile icon

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,69 +1,113 @@
 require 'spec_helper'
 
 describe ( 'users/show' ) {
-  subject { rendered }
-
   let ( :user ) { User.first }
+
+  subject { rendered }
 
   before {
     assign( :user, user )
-    session[ :browserid_email ] = user.email
-    assign( :current_user, user )
-
-    assign( :trays, user.trays )
     render
   }
 
-  it {
-    should have_css 'h1.collection-header', text: user.name
+  describe ( 'left bar' ) {
+    it {
+      should have_css '.left'
+    }
+
+    it {
+      should have_css '.left .holder.col-sidebar'
+    }
+
+    it {
+      should have_css '.col-sidebar .titlebar.main', text: 'User'
+    }
+
+    it {
+      should have_css '.col-sidebar .thumbnail'
+    }
+
+    it {
+      should have_css '.col-sidebar .stalking_menu_holder'
+    }
+
+    it {
+      should have_css '.stalking_menu_holder .stalking_menu'
+    }
   }
 
-  it {
-    should have_css 'dd', text: user.bio
-  }
+  describe ( 'main content' ) {
+    it {
+      should have_css '.expandable h1'
+    }
 
-  it {
-    should have_css 'section.user-trays'
-  }
+    it {
+      should have_css 'h1.collection-header', text: user.name
+    }
 
-  it {
-    should have_css 'h1.collection-header', text: 'Trays'
-  }
+    it {
+      should have_css 'dd', text: user.bio
+    }
 
-  it {
-    should have_css '.tray-preview'
-  }
+    it {
+      should_not have_css 'section.user-trays'
+    }
 
-  it {
-    should have_css %Q|a[href*="#{user_trays_path user}"]|, text: 'Tray Manager'
-  }
+    it {
+      should_not have_css 'section.user-circles'
+    }
 
-  it {
-    should have_css 'section.user-circles'
-  }
+    it {
+      should have_css 'section.user-collections'
+    }
 
-  it {
-    should have_css 'h1.collection-header', text: 'Circles'
-  }
+    it {
+      should have_css 'h1.collection-header', text: 'Collections'
+    }
 
-  it {
-    should have_css %Q|a[href*="#{circles_path}"]|, text: 'Circle Manager'
-  }
+    context ( 'with user signed in' ) {
+      before {
+        session[ :browserid_email ] = user.email
+        assign( :current_user, user )
 
-  it {
-    should have_css '.user-circles .GALLERY.circle-gallery'
-  }
+        render
+      }
 
-  it {
-    should have_css '.circle-gallery .gallery_item'
-  }
+      it {
+        should have_css 'section.user-trays'
+      }
 
-  it {
-    should have_css 'section.user-collections'
-  }
+      it {
+        should have_css 'h1.collection-header', text: 'Trays'
+      }
 
-  it {
-    should have_css 'h1.collection-header', text: 'Collections'
-  }
+      it {
+        should have_css '.tray-preview'
+      }
 
+      it {
+        should have_css %Q|a[href*="#{user_trays_path user}"]|, text: 'Tray Manager'
+      }
+
+      it {
+        should have_css 'section.user-circles'
+      }
+
+      it {
+        should have_css 'h1.collection-header', text: 'Circles'
+      }
+
+      it {
+        should have_css %Q|a[href*="#{circles_path}"]|, text: 'Circle Manager'
+      }
+
+      it {
+        should have_css '.user-circles .GALLERY.circle-gallery'
+      }
+
+      it {
+        should have_css '.circle-gallery .gallery_item'
+      }
+    }
+  }
 }

4f8097c23bedab6fd85b2d83d9f569bfcde2d03d
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-03-05 18:27:07 -0500

    #11578 - don't show non-public spotlights on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @works = Work.limit(25).order("RANDOM()")
     @collection = Collection.where(approved: true).limit(1).order("RANDOM()").first
-    @spotlights = Spotlight.limit(25).order("RANDOM()")
+    @spotlights = Spotlight.user_only.where( privacy: 'public' ).limit(25).order("RANDOM()")
     @all = (@works+@spotlights).shuffle
   end
   

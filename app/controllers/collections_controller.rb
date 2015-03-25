cb72d2a0e19157796121d1c9c7e63d5cc40fd825
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-03-25 13:12:43 -0400

    #10148 - allow collection to be set to public

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -94,6 +94,6 @@ class CollectionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def collection_params
-      params.require(:collection).permit(:name, :description, :configuration, :source)
+      params.require(:collection).permit(:name, :description, :configuration, :source, :approved)
     end
 end

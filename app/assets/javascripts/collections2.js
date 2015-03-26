a037fdf4d27462b1665cae03be7f9d9ce0ca2269
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-03-26 14:46:06 -0400

    #11713 - added delete button & warning

---
+++ app/assets/javascripts/collections2.js
@@ -0,0 +1,7 @@
+$( function() {
+  $( '.collections.show .delete-collection' ).click( function() {
+    if ( !confirm( 'Are you sure you want to delete this collection and all works and annotations within?' ) ) {
+      return false;
+    }
+  } );
+} );

978e50bb84f61cf3b7dccdd1c788c89cb92f1e0a
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-04-30 17:21:21 -0400

    #11262 - no longer used in about

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,12 @@
 class HomeController < ApplicationController
   def index
-    @works = Work.with_thumb.limit(25).order("RANDOM()")
+    @works = Work.approved.with_thumb.limit(25).order("RANDOM()")
     @collection = Collection.with_works.where(approved: true).limit(1).order("RANDOM()").first
     @spotlights = Spotlight.user_only.where( privacy: 'public' ).limit(25).order("RANDOM()")
     @all = (@works+@spotlights).shuffle
   end
   
   def about
-    @work = Work.limit(1).order("RANDOM()").first
-    while @work.images.none?
-      @work = Work.limit(1).order("RANDOM()").first
-    end
   end
 
   def bot

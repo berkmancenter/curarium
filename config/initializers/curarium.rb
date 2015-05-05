6a244eb5424ef6ea2783c405ed66aa503be2f6f1
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-05-05 11:42:38 -0400

    #12006 - pre-create thumbnail directory

--- config/initializers/curarium.rb
+++ config/initializers/curarium.rb
@@ -4,3 +4,7 @@ Curarium::ANNOTATION_THUMBNAILS_FOLDER = 'annotation_thumbnails'
 Curarium::ANNOTATION_THUMBNAILS_PATH = Rails.public_path.join( Curarium::ANNOTATION_THUMBNAILS_FOLDER )
 
 Curarium::BOT_UA = 'CurariumBot/0.4.8 (+http://curarium.com/bot)'
+
+# make directory for work thumbnails
+FileUtils.mkpath Rails.public_path.join( 'thumbnails', 'works' ).to_s
+

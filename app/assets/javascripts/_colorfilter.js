bd702683588b3c20117617e6d87f53c42649e16e
priyankasuresh <priyanka.suresh16@gmail.com>
2015-05-07 13:04:58 -0400

    Front-end for Colorviz with horizontal bar graphs

--- app/assets/javascripts/_colorfilter.js
+++ app/assets/javascripts/_colorfilter.js
@@ -1,10 +1,11 @@
 $( function( ) { 
-	colone = document.getElementById("one_color").innerHTML;
-	document.getElementById("one_color").style.backgroundColor= colone;
-	coltwo = document.getElementById("two_color").innerHTML;
-	document.getElementById("two_color").style.backgroundColor= coltwo;
-	colthree = document.getElementById("three_color").innerHTML;
-	document.getElementById("three_color").style.backgroundColor= colthree;
-	colfour = document.getElementById("four_color").innerHTML;
-	document.getElementById("four_color").style.backgroundColor= colfour;
+	var i,j;
+
+	var x = document.getElementsByClassName("color-info");
+	var y = document.getElementsByClassName("color-percent");
+
+		for(i = 0, j = 0; (j < y.length) && (i < x.length); i++, j++){
+			x[i].style.backgroundColor = x[i].innerHTML;
+			x[i].style.width = y[j].innerHTML+"px";
+			}
 });

01294e882052f0bde6c03b7c355e436ddd5e8bfb
priyankasuresh <priyanka.suresh16@gmail.com>
2015-05-06 22:05:49 -0400

    Adding front end

---
+++ app/assets/javascripts/_colorfilter.js
@@ -0,0 +1,10 @@
+$( function( ) { 
+	colone = document.getElementById("one_color").innerHTML;
+	document.getElementById("one_color").style.backgroundColor= colone;
+	coltwo = document.getElementById("two_color").innerHTML;
+	document.getElementById("two_color").style.backgroundColor= coltwo;
+	colthree = document.getElementById("three_color").innerHTML;
+	document.getElementById("three_color").style.backgroundColor= colthree;
+	colfour = document.getElementById("four_color").innerHTML;
+	document.getElementById("four_color").style.backgroundColor= colfour;
+});

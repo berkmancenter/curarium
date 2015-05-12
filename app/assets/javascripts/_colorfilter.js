f352837a7acd160b8330219488dbeff68e5904a0
priyankasuresh <priyanka.suresh16@gmail.com>
2015-05-12 16:27:30 -0400

    Color Viz front-end changes

--- app/assets/javascripts/_colorfilter.js
+++ app/assets/javascripts/_colorfilter.js
@@ -6,6 +6,7 @@ $( function( ) {
 
 		for(i = 0, j = 0; (j < y.length) && (i < x.length); i++, j++){
 			x[i].style.backgroundColor = x[i].innerHTML;
-			x[i].style.width = y[j].innerHTML+"px";
+			x[i].style.color = x[i].innerHTML;
+			x[i].style.width = (y[j].innerHTML)*2+"%";
 			}
 });

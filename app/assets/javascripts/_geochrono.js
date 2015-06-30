1c7325f797e6e26e152d4e680d4634d4eddb66f5
priyankasuresh <priyanka.suresh16@gmail.com>
2015-06-30 19:58:49 +0200

    Prelim timeline stuff

--- app/assets/javascripts/_geochrono.js
+++ app/assets/javascripts/_geochrono.js
@@ -4,28 +4,11 @@ function FixMargin(left) {
 
 $(document).ready(function( ) { 
   $("#left-button").click(function(){
-        $(".chrono-info").css('margin-left','+=274px');
+        $(".chrono-info").css('margin-left','+=270px');
   });
 
    $("#right-button").click(function( ) {
-        $(".chrono-info").css('margin-left','-=274px');
+        $(".chrono-info").css('margin-left','-=270px');
 
   });
 });
-
-$.ajax( {
-  url: '/public/geochrono/query/' + $( '#works-geochrono' ).data( 'queryId' ) + '/data.json',
-  success: function( ids ) {
-    // render visualization
-  
-  var data = result;
-
-  var firstWork = data[ 0 ];
-
-  var firstWorkId = firstWork[ 0 ];
-
-  var firstWorkDateStart = Date.parse( firstWork[ 1 ] );
-
-  var firstWorkDateEnd = Date.parse( firstWork[ 2 ] );
-  }
-} );

27718e430f495d7f0173eba2241e260235836954
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-07-06 22:38:51 -0400

    #11847 - reformat

--- app/assets/javascripts/_colorfilter.js
+++ app/assets/javascripts/_colorfilter.js
@@ -1,15 +1,15 @@
 $( function( ) { 
-	var i, j, k;
+  // TODO: Note, this can all be handled server-side
+  var i, j, k;
 
-	var x = document.getElementsByClassName("color-info");
-	var y = document.getElementsByClassName("color-percent");
-	var z = document.getElementsByClassName("color-hex");
+  var x = document.getElementsByClassName("color-info");
+  var y = document.getElementsByClassName("color-percent");
+  var z = document.getElementsByClassName("color-hex");
 
-		for(i = 0, j = 0, k=0; (j < y.length) && (i < x.length) && (k < z.length); i++, j++, k++){
-			x[i].style.backgroundColor = x[i].innerHTML;
-			x[i].style.color = x[i].innerHTML;
-			x[i].style.width = (y[j].innerHTML)+"%";
-			x[i].title = z[k].innerHTML;
-			}
-
+  for(i = 0, j = 0, k=0; (j < y.length) && (i < x.length) && (k < z.length); i++, j++, k++){
+    x[i].style.backgroundColor = x[i].innerHTML;
+    x[i].style.color = x[i].innerHTML;
+    x[i].style.width = (y[j].innerHTML)+"%";
+    x[i].title = z[k].innerHTML;
+  }
 });

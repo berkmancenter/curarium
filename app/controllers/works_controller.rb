cdc982480165c04a1b2e45d4eb42be5ea989e7e8
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-07-10 12:45:34 -0400

    #11848 - upcase required for non-numeric colors

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -80,7 +80,7 @@ class WorksController < ApplicationController
     @colorfilter = nil
     if params[ :colorfilter ].present?
       @colorfilter = params[ :colorfilter ]
-      where_clause << " AND top_colors::text like '%#{ActiveRecord::Base.send :sanitize_sql_array, [ '%s', @colorfilter ]}%'"
+      where_clause << " AND top_colors::text like '%#{ActiveRecord::Base.send :sanitize_sql_array, [ '%s', @colorfilter.remove('#').upcase ]}%'"
       have_query = true
     end
 

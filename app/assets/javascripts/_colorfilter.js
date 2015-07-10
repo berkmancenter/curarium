6a1f8e2d3091293dd378de66ee32bde212ab11a9
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-07-10 10:58:35 -0400

    #11848 - reimplement colorfilter with no JS, more partials, & correct scaling

--- app/assets/javascripts/_colorfilter.js
+++ app/assets/javascripts/_colorfilter.js
@@ -1,18 +1,4 @@
 $( function( ) { 
-  // TODO: Note, this can all be handled server-side
-  var i, j, k;
-
-  var x = document.getElementsByClassName("color-info");
-  var y = document.getElementsByClassName("color-percent");
-  var z = document.getElementsByClassName("color-hex");
-
-  for(i = 0, j = 0, k=0; (j < y.length) && (i < x.length) && (k < z.length); i++, j++, k++){
-    x[i].style.backgroundColor = x[i].innerHTML;
-    x[i].style.color = x[i].innerHTML;
-    x[i].style.width = (y[j].innerHTML)+"%";
-    x[i].title = z[k].innerHTML;
-  }
-
   $( '.color-viz a.thumbnail' ).click( function( ) {
     $.get( $( this ).prop( 'href' ) , function( popupHtml ) {
       $.magnificPopup.open( {
@@ -26,5 +12,4 @@ $( function( ) {
 
     return false;
   } );
-
-});
+} );

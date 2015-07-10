ba265dc782621f3b55aedd4b27438d1706c4abe4
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-07-10 12:45:46 -0400

    #11848 - clicking a color finally searches

--- app/assets/javascripts/_colorfilter.js
+++ app/assets/javascripts/_colorfilter.js
@@ -1,5 +1,5 @@
 $( function( ) { 
-  $( '.color-viz a.thumbnail' ).click( function( ) {
+  $( '.colorfilter a.thumbnail' ).click( function( ) {
     $.get( $( this ).prop( 'href' ) , function( popupHtml ) {
       $.magnificPopup.open( {
         showCloseBtn: false,
@@ -10,6 +10,13 @@ $( function( ) {
       } );
     } );
 
+    return false;
+  } );
+
+  $( '.colorfilter a.color-info' ).click( function( ) {
+    var visForm = $( '#vis-form' );
+    visForm.find( '#colorfilter' ).attr( 'disabled', false ).val( $( this ).data( 'color' ) );
+    visForm.submit( );
     return false;
   } );
 } );

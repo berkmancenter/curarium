805d0640009ddc2e5766e03f235f1e3b090e730a
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-07-16 14:36:13 -0400

    #10914 - user thumb redux

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,6 @@ module UsersHelper
   # Returns the Gravatar for the given user.
   def gravatar_for( user )
     gravatar_id = Digest::MD5::hexdigest user.email.downcase
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag gravatar_url, alt: user.name, class: 'gravatar'
+    "https://secure.gravatar.com/avatar/#{gravatar_id}"
   end
 end
